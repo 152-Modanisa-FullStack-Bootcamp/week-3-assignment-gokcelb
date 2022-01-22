d9c35ba6c06f03986c0b482ba17b7980416fe0db
gokcelb <gokcelbilgin@gmail.com>
2022-01-23 00:08:14 +0300

    Add title check and class binding tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -7,26 +7,26 @@ localVue.use(Vuex)
 
 
 describe("App", () => {
+    let wrapper
+    let store
+    let state
+    let getters
+
+    beforeEach(() => {
+        getters = {
+            getCount: jest.fn()
+        }
+        state = {
+            count: 0
+        }
+        store = {
+            state,
+            getters
+        }
+        wrapper = shallowMount(App, { store, localVue })
+    })
+
     describe("Elements exist check", () => {
-        let wrapper
-        let store
-        let state
-        let getters
-
-        beforeEach(() => {
-            getters = {
-                getCount: jest.fn()
-            }
-            state = {
-                count: 0
-            }
-            store = {
-                state,
-                getters
-            }
-            wrapper = shallowMount(App, { store, localVue })
-        })
-
         test("Component exists", () => {
             expect(wrapper.exists()).toBeTruthy()
         })
@@ -37,26 +37,36 @@ describe("App", () => {
         })
     })
 
-    const testCases = [
-        { count: 10, message: "Danger!!! Case count is 10k" },
-        { count: 5, message: "Life is normal. Case count is 5k" },
-        { count: 2, message: "So safe. Case count is 2k" }
-    ]
-    for(let testCase of testCases) {
-        test("Message changes according to count value", () => {
-            const state = {
-                count: testCase.count
-            }
-            const getters = {
-                getCount: jest.fn()
-            }
-            const store = {
-                state,
-                getters
-            }
-            const wrapper = shallowMount(App, { store, localVue })
-            const msg = wrapper.find("div.notificationArea").text()
-            expect(msg).toBe(testCase.message)
-        })
-    }
+    test("Title is 'Daily Corona Cases in Turkey'", () => {
+        const title = wrapper.find("h1")
+        expect(title.text()).toBe("Daily Corona Cases in Turkey")
+    })
+
+    describe("Dynamic tests", () => {
+        const testCases = [
+            { count: 10, message: "Danger!!! Case count is 10k", class: "danger" },
+            { count: 5, message: "Life is normal. Case count is 5k", class: "normal" },
+            { count: 2, message: "So safe. Case count is 2k", class: "safe" }
+        ]
+        for(let testCase of testCases) {
+            test("Message and class change according to count", async () => {
+                const state = {
+                    count: testCase.count
+                }
+                const store = {
+                    state,
+                }
+                const computed = {
+                    getCount: () => state.count
+                }
+                const wrapper = shallowMount(App, { store, computed, localVue })
+                const notifArea = wrapper.find("div.notificationArea")
+                const msg = notifArea.text()
+                const notifClasses = notifArea.classes()
+                
+                expect(msg).toBe(testCase.message)
+                expect(notifClasses).toContain(testCase.class)
+            })
+        }
+    })
 })
